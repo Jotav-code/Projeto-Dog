07ca5fff63cb3c3c750387e55e82249fb928c924
joao vitor <joaovitor646260@gmail.com>
2025-09-22 21:03:20 -0300

    Chore: construindo o header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Style from './Header.module.css';
 import { Link } from 'react-router-dom';
+import DogsIcon from '/src/Assets/dogs.svg?react';
+
 const Header = () => {
   return (
     <header className={Style.header}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="login">Login / Criar</Link>
+      <nav className={`${Style.nav} container`}>
+        <Link className={Style.logo} to="/" aria-label="Dogs - Header">
+          <DogsIcon />
+        </Link>
+        <Link className={Style.login} to="login">
+          Login / Criar
+        </Link>
       </nav>
     </header>
   );
