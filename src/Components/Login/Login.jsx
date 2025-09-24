189d966be555df4e12790841e9ecd0b58ba507eb
joao vitor <joaovitor646260@gmail.com>
2025-09-24 17:22:32 -0300

    feat: criação de pagina de cadastro

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -12,14 +12,16 @@ const Login = () => {
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="resetar-senha" element={<LoginPasswordReset />} />
-        <Route path="esqueceu-senha" element={<LoginPasswordLost />} />
-      </Routes>
-    </div>
+    <section className={Style.login}>
+      <div className={Style.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="resetar-senha" element={<LoginPasswordReset />} />
+          <Route path="esqueceu-senha" element={<LoginPasswordLost />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
--- src/Components/Login/LoginCreate/LoginCreate.jsx
+++ src/Components/Login/LoginCreate/LoginCreate.jsx
@@ -1,7 +1,48 @@
+import { func } from 'prop-types';
 import React from 'react';
+import Input from '../../Form/Input/Input';
+import useForm from '../../../Hook/useForm';
+import Button from '../../Form/Button/Button';
+import { POST_USER } from '../../../api';
+import { UserContext } from '../../../UserContext';
+import Error from '../../Helper/Error';
 
 const LoginCreate = () => {
-  return <div>Criar</div>;
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  const { userLogin, loading, error } = React.useContext(UserContext);
+
+  const { url, options } = POST_USER({
+    username: username.value,
+    email: email.value,
+    password: password.value,
+  });
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await fetch(url, options);
+    const json = await response.json();
+
+    if (response.ok) userLogin(username.value, password.value);
+  }
+  return (
+    <section className="animeLeft">
+      <h1 className="titulo">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" label="Usuário" {...username} />
+        <Input type="email" label="E-mail" {...email} />
+        <Input type="password" label="Senha" {...password} />
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastre-se</Button>
+        )}
+        <Error erro={error} />
+      </form>
+    </section>
+  );
 };
 
 export default LoginCreate;
