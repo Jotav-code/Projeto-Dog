7f6d9030cb286c40d366cce1527c354086b5920d
joao vitor <joaovitor646260@gmail.com>
2025-10-01 20:25:59 -0300

    feat: criando footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Style from './Footer.module.css';
+import Dogs from '../../Assets/dogs-footer.svg?react';
 const Footer = () => {
-  return <footer className={Style.footer}></footer>;
+  return (
+    <footer className={Style.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos resevados</p>
+    </footer>
+  );
 };
 
 export default Footer;
