786d81439b2946e5b5b041a508aa0d7dd474c7a1
joao vitor <joaovitor646260@gmail.com>
2025-10-01 21:48:56 -0300

    feat: criação ded pagina de estatistica com bib de graph

--- src/Components/Login/User/UserStats.jsx
+++ src/Components/Login/User/UserStats.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
+import useFetch from '../../../Hook/useFetch';
+import { STATS_GET } from '../../../api';
+import Error from '../../Helper/Error';
+import Loading from '../../Helper/Loading';
+import UserStatsGraphs from './UserStatsGraphs';
+import Head from '../../Helper/Head';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estatisticas" />
-      estastisticas
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getStats() {
+      const { url, options } = STATS_GET();
+      request(url, options);
+    }
+    getStats();
+  }, [request]);
+
+  if (error) return <Error />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <div>
+        <Head title="Estatisticas" />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
